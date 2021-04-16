9c048e0689cd7333b1c538ece31ca213f6979446
namChul <knc219@naver.com>
2021-04-16 13:41:20 +0900

    20210416중간저장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -20,49 +22,70 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            //Member findMember = em.find(Member.class, 1L);
-            //findMember.setName("HelloJPA");
-            //commit 직전 변경사항이 있으면 update쿼리를 날려서 update 함.
-
-            //비영속
-
-            //영속: 영속성 컨텍스트에 포함되어 관리 되는 상태
-
-            //저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
-            Member member = new Member();
-            member.setUsername("Member1");
             //연관관계의 주인에 값을 세팅해주는 부분 Member 와 Team 객체의 연관관계의 주인은 Member.team 이다
             //setTeam -> changeTeam 으로 바꿔주면서 team을 셋팅할때 team.getMembers().add(this) 를 해줌으로
             //양방향으로 셋팅을 하는 메서드로 만들어준다 (연관관계 편의 메서드라고 한다.)
-            member.changeTeam(team);
-            team.addMember(member);
-            em.persist(member);
 
             //연관관계의 주인은 아니지만 둘다 셋팅해주는게 좋다.
             //DB에서 읽지않고 영속성 컨텍스트의 순수 객체를 읽을때 못 읽어오는 경우가 있기 떄문
             //team.getMembers().add(member);
 
-//            em.flush();
-//            em.clear();
 
-            Team findTeam = em.find(Team.class, team.getId());
-            List<Member> members = findTeam.getMembers();
+            //구현클래스마다 테이블 전략 -> 안쓰는게 좋음 이런식으로 부모클래스로 조회시 모든 테이블을 union 해서 성능저하
+            //보통 조인 전략을 기본으로 사용한다.
+            //정말 단순하고 데이터도 얼마 없을 시 단일 테이블 전략 사용
 
-            System.out.println("===========================");
-            for (Member m : members) {
-                System.out.println("m.get = " + m.getUsername());
-            }
-            System.out.println("===========================");
+            //MappedSuperClass 는 엔티티가 아니기 때문에 조회가 안된다.
+
+            Team team = new Team();
+            team.setName("A");
+            em.persist(team);
+
+            Team teamB = new Team();
+            teamB.setName("B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setTeam(team);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+
+            Member m = em.find(Member.class,member2.getId());
+
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
+            e.printStackTrace();
         }finally {
             em.close();
         }
         emf.close();
+    }
+
+    private static void logic(Member m1, Member m2) {
+        System.out.println("m1 instanceof Member = " + (m1 instanceof Member));
+        System.out.println("m2 instanceof Member = " + (m2 instanceof Member));
+    }
+
+    private static void printMember(Member member) {
+        member.getUsername();
+        System.out.println("member = " + member.getUsername());
+    }
+
+    private static void printMemberAndTeam(Member member) {
+        String username = member.getUsername();
+        System.out.println("username = " + username);
+        Team team = member.getTeam();
+        System.out.println("team = " + team.getName());
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,13 +1,14 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 
 //@Table(name = "USER") //DB TEBLE 이름으로 바꾸고싶을때 기본은 클래스명과 같은 테이블을 찾는다.
 @Entity
-public class Member {
+public class Member extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -22,13 +23,6 @@ public class Member {
     @ManyToOne(fetch = FetchType.LAZY) // 해당 객체가 M 매핑되는 객체가 1
     @JoinColumn(name = "TEAM_ID") //조인해야하는 컬럼 명
     private Team team;
-
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<MemberProduct>();
 
     public Long getId() {
         return id;
@@ -58,6 +52,5 @@ public class Member {
         this.team = team;
         team.getMembers().add(this); // 팀을 셋팅함과 동시에 해당 팀에 자신(Member)를 추가해준다.
     }
-
 
 }
